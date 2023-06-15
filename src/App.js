225d68b3dff6e740b4f397fb3060479dfb83826d
pashupati-singh <119448686+pashupati-singh@users.noreply.github.com>
2023-06-15 22:18:51 +0530

    day-3

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@
 import "./App.css";
 import { Navbar } from "./Components/Navbar";
 import { Contact } from './Pages/Contact';
-// import { FAQ } from './Pages/FAQ';
-// import { Login } from './Pages/Login';
+
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      {/* <Navbar/> */}
+      <Contact ></Contact>
     </div>
   );
 }
